be2f3853dc779f6ce040dc7a8b8021f11f92d700
tr3dh <timalrot@gmail.com>
2026-02-05 01:00:10 +0100

    Samples erweitert

--- Import/juliaScripts/autodiff_unwrappedsigmaTSM_t.jl
+++ Import/juliaScripts/autodiff_unwrappedsigmaTSM_t.jl
@@ -1,7 +1,6 @@
 # Julia Skript
 #
 # unique external nodes :
-# | arg '__INVALID__', order [0], dimensions {}
 # | arg 'E0', order [2], dimensions {3, 3}
 # | arg 'D', order [2], dimensions {3, 3}
 # | arg 'epsilon', order [1], dimensions {3}
@@ -9,15 +8,9 @@
 # | arg 'depsilonv1_dD1', order [3], dimensions {3, 3, 3}
 # | arg 'depsilonv2_dD2', order [5], dimensions {3, 3, 3, 3, 3}
 # | arg 'depsilonv3_dD3', order [7], dimensions {3, 3, 3, 3, 3, 3, 3}
-# | arg 'zeros_ord4_dm3333', order [4], dimensions {3, 3, 3, 3}
 # | arg 'Identity_ord4_dm3333', order [4], dimensions {3, 3, 3, 3}
-# | arg 'zeros_ord3_dm333', order [3], dimensions {3, 3, 3}
 # | arg 'depsilonv4_dD4', order [9], dimensions {3, 3, 3, 3, 3, 3, 3, 3, 3}
-# | arg 'zeros_ord6_dm333333', order [6], dimensions {3, 3, 3, 3, 3, 3}
-# | arg 'zeros_ord5_dm33333', order [5], dimensions {3, 3, 3, 3, 3}
 # | arg 'depsilonv5_dD5', order [11], dimensions {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3}
-# | arg 'zeros_ord8_dm33333333', order [8], dimensions {3, 3, 3, 3, 3, 3, 3, 3}
-# | arg 'zeros_ord7_dm3333333', order [7], dimensions {3, 3, 3, 3, 3, 3, 3}
 # | arg 'depsilonv6_dD6', order [13], dimensions {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3}
 
 using LinearAlgebra
@@ -76,9 +69,8 @@ function create_eps(dims::Integer...)
 end
 
 
-function autodiff_sigmaTSM_t(__INVALID__, E0, D, epsilon, epsilonv, depsilonv1_dD1, depsilonv2_dD2, depsilonv3_dD3, depsilonv4_dD4, depsilonv5_dD5, depsilonv6_dD6)
+function autodiff_sigmaTSM_t(E0, D, epsilon, epsilonv, depsilonv1_dD1, depsilonv2_dD2, depsilonv3_dD3, depsilonv4_dD4, depsilonv5_dD5, depsilonv6_dD6)
 
-	@assert ndims(__INVALID__) == 0
 	@assert size(E0) == (3, 3)
 	@assert size(D) == (3, 3)
 	@assert length(epsilon) == 3
@@ -86,15 +78,9 @@ function autodiff_sigmaTSM_t(__INVALID__, E0, D, epsilon, epsilonv, depsilonv1_d
 	@assert size(depsilonv1_dD1) == (3, 3, 3)
 	@assert size(depsilonv2_dD2) == (3, 3, 3, 3, 3)
 	@assert size(depsilonv3_dD3) == (3, 3, 3, 3, 3, 3, 3)
-	zeros_ord4_dm3333 = create_zeros(3, 3, 3, 3)
 	Identity_ord4_dm3333 = create_Identity(3, 3, 3, 3)
-	zeros_ord3_dm333 = create_zeros(3, 3, 3)
 	@assert size(depsilonv4_dD4) == (3, 3, 3, 3, 3, 3, 3, 3, 3)
-	zeros_ord6_dm333333 = create_zeros(3, 3, 3, 3, 3, 3)
-	zeros_ord5_dm33333 = create_zeros(3, 3, 3, 3, 3)
 	@assert size(depsilonv5_dD5) == (3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3)
-	zeros_ord8_dm33333333 = create_zeros(3, 3, 3, 3, 3, 3, 3, 3)
-	zeros_ord7_dm3333333 = create_zeros(3, 3, 3, 3, 3, 3, 3)
 	@assert size(depsilonv6_dD6) == (3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3)
 
 
@@ -102,7 +88,7 @@ function autodiff_sigmaTSM_t(__INVALID__, E0, D, epsilon, epsilonv, depsilonv1_d
 end
 
 start_time = time()
-res = autodiff_sigmaTSM_t(rand(), rand(3, 3), rand(3, 3), rand(3), rand(3), rand(3, 3, 3), rand(3, 3, 3, 3, 3), rand(3, 3, 3, 3, 3, 3, 3), rand(3, 3, 3, 3, 3, 3, 3, 3, 3), rand(3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3), rand(3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3))
+res = autodiff_sigmaTSM_t(rand(3, 3), rand(3, 3), rand(3), rand(3), rand(3, 3, 3), rand(3, 3, 3, 3, 3), rand(3, 3, 3, 3, 3, 3, 3), rand(3, 3, 3, 3, 3, 3, 3, 3, 3), rand(3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3), rand(3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3))
 elapsed = time() - start_time
 println("Laufzeit: ", elapsed, " s")
 println("Ergebnis: ", res)
