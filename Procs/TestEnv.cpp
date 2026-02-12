ed97adef8156439f4eacc5ef1aba6bc4a5b84d95
tr3dh <timalrot@gmail.com>
2026-02-12 21:46:53 +0100

    mehrere Basisfunktionen für erweiterte automatische Funktionsregistrierung getestet

--- Procs/TestEnv.cpp
+++ Procs/TestEnv.cpp
@@ -1,6 +1,92 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <typeinfo>
 
-int main(){
+#include "RegistryLanguage/Interpreter.h"
+
+//
+template<typename Ret, typename... Args>
+void iterateTypes(Ret(*func)(Args...)) {
+    
+    {
+        using ReturnType = Ret;
+        std::cout << "Return: " << typeid(ReturnType).name() << endl;
+    }
+    
+    size_t index = 0;
+    ([&]<typename T>() {
+        using ParamType = T;
+        std::cout << "Param " << index++ << ": " << typeid(ParamType).name() << endl;
+    }.template operator()<Args>(), ...);
+}
+
+//
+template<typename Class, typename Ret, typename... Args>
+void iterateTypes(Ret(Class::*func)(Args...)) {
+
+    {
+        using ReturnType = Ret;
+        std::cout << "Return: " << typeid(ReturnType).name() << endl;
+    }
+    
+    size_t index = 0;
+    ([&]<typename T>() {
+        using ParamType = T;
+        std::cout << "Param " << index++ << ": " << typeid(ParamType).name() << endl;
+    }.template operator()<Args>(), ...);
+}
+
+//
+template<typename Class, typename Ret, typename... Args>
+void iterateTypes(Ret(Class::*func)(Args...) const) {
+    std::cout << "Member-Funktion (const)" << endl;
+    
+    {
+        using ReturnType = Ret;
+        std::cout << "Return: " << typeid(ReturnType).name() << endl;
+    }
+    
+    size_t index = 0;
+    ([&]<typename T>() {
+        using ParamType = T;
+        std::cout << "Param " << index++ << ": " << typeid(ParamType).name() << endl;
+    }.template operator()<Args>(), ...);
+}
+
+//
+class MyClass {
+public:
+
+    int memberFunc(double x, std::string y) {
+        return static_cast<int>(x);
+    }
+    
+    float constMemberFunc(int a) const {
+        return static_cast<float>(a);
+    }
+    
+    static std::string staticFunc(int a, double b) {
+        return "test";
+    }
+};
+
+// Freie Funktion
+void freeFunc(int a, float b) {}
+
+int main() {
+
+    std::cout << "=== Freie Funktion ===" << endl;
+    iterateTypes(&freeFunc);
+    
+    std::cout << "\n=== Statische Member-Funktion ===" << endl;
+    iterateTypes(&MyClass::staticFunc);
+    
+    std::cout << "\n=== Member-Funktion ===" << endl;
+    iterateTypes(&MyClass::memberFunc);
+    
+    std::cout << "\n=== Const Member-Funktion ===" << endl;
+    iterateTypes(&MyClass::constMemberFunc);
 
     return 0;
 }
