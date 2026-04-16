9c87ad02a1b0d9571d6ae42e7bd1f5dacf8bc978
tr3dh <timalrot@gmail.com>
2026-04-16 18:14:23 +0200

    Extension Export Skript angepasst

--- Batch/buildExtension.bat
+++ Batch/buildExtension.bat
@@ -11,15 +11,15 @@ make
 
 echo Extension gebaut und gepackt
 
-mkdir ..\..\__OUT\extension
+mkdir ..\..\__OUT\VSCodeExtension
 
 REM Kopiere die gepackte *.vsix nach __OUT
-copy "*.vsix" "..\..\__OUT\extension\" /Y
+copy "*.vsix" "..\..\__OUT\VSCodeExtension\" /Y
 
 REM Dateinamen der *.vsix speicher
 for %%f in ("*.vsix") do set VSIX_NAME=%%~nxf
 
 del "*.vsix"
 
-echo code --install-extension "%VSIX_NAME%" > "..\..\__OUT\extension\installExtension.bat"
-echo code --uninstall-extension tr3dh.baldur > "..\..\__OUT\extension\uninstallExtension.bat"
+echo code --install-extension "%VSIX_NAME%" > "..\..\__OUT\VSCodeExtension\installExtension.bat"
+echo code --uninstall-extension tr3dh.baldur > "..\..\__OUT\VSCodeExtension\uninstallExtension.bat"
